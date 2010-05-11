9ba7d3028f50256df43c30701db1b21c8f4e09e9
Craig McNamara <Craig.McNamara@gmail.com>
2010-05-11 17:19:03 -0400

    Added capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,23 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+ssh_options[:forward_agent] = true
+set :application, "Proactive Homepage"
+set :repository,  "git@github.com:M4N14C/Proactive-Technologies.git"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
+#Old Git Version on App Server
+set :scm_verbose, true
+# set :git_shallow_clone, 1
+# set :git_enable_submodules, 1
+set :branch, "master"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user, "craigmpw"
+set :password, "tpqf58a1"
+
+set :deploy_to, "/home/craigmpw/apps/#{application}"
+
+role :web, "proactive.mpwstaging.com"                          # Your HTTP server, Apache/etc
+role :app, "proactive.mpwstaging.com"                          # This may be the same as your `Web` server
+role :db,  "mysql.mpwstaging.com", :primary => true # This is where Rails migrations will run
+
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need

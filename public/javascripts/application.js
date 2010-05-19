c6c8ec531dd9a3bb5e2a665126af578228bb37d2
Craig McNamara <Craig.McNamara@gmail.com>
2010-05-19 02:13:01 -0400

    Final Scroll and animation changes.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,3 +5,15 @@ function show_slides(){
 	$('#wrapper').slideDown();
 };
 
+$(document).ready(function(){
+	$('#slideshow').bxCarousel({
+		display_num: 5,
+		auto_interval: 2000,
+		speed: 1000,
+		move: 1,
+		auto: true,
+		controls: false,
+		margin: 0,
+		auto_hover: true
+	});
+});

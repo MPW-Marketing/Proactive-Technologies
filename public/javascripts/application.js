0ea5c122c05bf24e33f18eb79fe59c19f801e9f4
Craig McNamara <Craig.McNamara@gmail.com>
2010-05-19 00:42:14 -0400

    Improved Animations.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,49 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 function show_slides(){
-	$('#summary').hide('blind', null, 500, null);
-	$('#wrapper').show('blind', null, 4000, null);
-	init_slides();
+	$('#summary').slideUp();
+	$('#wrapper').slideDown();
 };
-
-function init_slides(){
-	$('#slideshow').serialScroll({
-		items:'li',
-		duration:4000,
-		stop:false,
-		start:0,
-		lazy:true,
-		easing:'linear',
-		interval:1,
-		exclude:3
-		// onBefore: on_before,
-		// onAfter: on_after
-	});
-};
-
-function on_before( e, elem, $pane, $items, pos ){
-	// $('#slides li:first-child').clone().appendTo('#slides');
-};
-
-function on_after(element, slideshow){
-	// $('#slides li:first-child').hide().remove();
-	// $('#slideshow').scrollTo($('#slides li:first-child'));
-	// $('#slideshow').trigger( 'start' );
-};
-
-jQuery(function( $ ){
-	
-	init_slides();
-	
-	$('#slideshow').bind('mouseenter', function(){
-		$(this).trigger( 'stop' );
-	});
-	
-	$('#slideshow').bind('mouseleave', function(){
-		$(this).trigger( 'start' );
-	});
-	
-	$('#summary').bind('click', show_slides);
-	
-});
 

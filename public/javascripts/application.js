4ce79ae82d14bb508a3c47bfea63a74afc34b67c
Craig McNamara <Craig.McNamara@gmail.com>
2010-07-27 15:28:38 -0400

    Changed to the sound free flash banner

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,7 +5,34 @@ function show_slides(){
 	$('#wrapper').slideDown();
 };
 
+function stop_music(){
+	$('#jsPlayer').jPlayer("stop");
+	$('#pause_btn').hide();
+	$('#play_btn').show();
+}
+
+function play_music(){
+	$('#jsPlayer').jPlayer("play");
+	$('#pause_btn').show();
+	$('#play_btn').hide();
+}
+
+function onOpenShadowbox(element){
+	stop_music();
+	return true;
+}
+
+function onCloseShadowbox(element){
+	play_music();
+	return true;
+}
+
 $(document).ready(function(){
+	Shadowbox.init({
+		onFinish: onOpenShadowbox,
+		onClose: onCloseShadowbox
+	});
+	$('#play_btn').hide();
 	$('#slideshow').bxCarousel({
 		display_num: 5,
 		auto_interval: 1,
@@ -16,4 +43,19 @@ $(document).ready(function(){
 		margin: 0,
 		auto_hover: true
 	});
+
+	$('#jsPlayer').jPlayer({
+		ready: function(){
+			this.element.jPlayer("setFile", "prosound.mp3").jPlayer("play");
+		},
+		volume: 30
+	});
+	$('#jsPlayer').jPlayer("onSoundComplete", function() {
+	  this.play(); // Auto-Repeat
+	});
+	
+});
+
+$("#about_link > a").click(function(){
+	alert("Stop");
 });

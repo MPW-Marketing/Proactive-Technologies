bdd970bc30f633766167521cb534509e452169f9
Damon Maldonado <damon@mpwmarketing.com>
2014-09-22 20:29:03 -0400

    7

--- config/boot.rb
+++ config/boot.rb
@@ -105,6 +105,9 @@ module Rails
     end
   end
 end
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 # All that for this:
 Rails.boot!
